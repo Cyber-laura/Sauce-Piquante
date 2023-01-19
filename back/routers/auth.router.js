d14194df1ddd9152d733a86a1c7096465f83b793
Laura <105425865+Cyber-laura@users.noreply.github.com>
2023-01-19 15:36:09 +0100

    MODIFICATION-USER-ID

--- back/routers/auth.router.js
+++ back/routers/auth.router.js
@@ -1,10 +1,8 @@
 const { createUser, login } = require("../controllers/users");
-
 const express = require("express")
 const authRouter = express.Router()
 
 authRouter.post("/signup", createUser);
 authRouter.post("/login", login);
 
-
-module.exports = {authRouter}
+module.exports = { authRouter }
